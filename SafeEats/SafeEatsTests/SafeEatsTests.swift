d578a3eb747f946beb352af18482de49c9291fcb
Edison <77561470+edison16a@users.noreply.github.com>
2024-11-14 16:37:36 -0800

    added more

---
+++ SafeEats/SafeEatsTests/SafeEatsTests.swift
@@ -0,0 +1,17 @@
+//
+//  SafeEatsTests.swift
+//  SafeEatsTests
+//
+//  Created by Edison Law on 11/13/24.
+//
+
+import Testing
+@testable import SafeEats
+
+struct SafeEatsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
